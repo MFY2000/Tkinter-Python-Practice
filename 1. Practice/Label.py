0e02bc927baaa8678d15fa80385dc745421acb3f
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-06 23:10:22 +0500

    MFY auto commit at 06/11/2021 23:10:04

---
+++ 1. Practice/Label.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+main = Tk()
+myLabel = Label(main,text="Hello World, I am Muhammad Fahad")
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+Label(main,text="Hello World").pack()
+myLabel.pack()
+
+if __name__ == '__main__':
+    main.mainloop()
